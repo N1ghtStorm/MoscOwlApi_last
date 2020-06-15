ac49b6afd563511faae9a108b0876a1c7371c0e0
N1ghtStorm <bushuevdv@mail.ru>
2020-06-15 18:16:22 +0300

    add base controller,repository and some models

---
+++ src/MoscOwlAPI/Domain/Models/Competition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoscOwlAPI.Domain.Models
+{
+    public class Competition
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ src/MoscOwlAPI/Domain/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoscOwlAPI.Data
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
